32dd92b26e565d908163cfa013b906a543b51a90
qingzhixing <qzsptmail@protonmail.com>
2023-11-09 21:52:40 +0800

    数据处理

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <graphics.h>
 #include "Tictactoe.h"
+#include <string>
+using std::string;
+
 int main() {
 	initgraph(600, 600);
 	BeginBatchDraw();
@@ -14,8 +17,23 @@ int main() {
 		}
 
 		// 处理数据
+		auto winner = tictactoe.CheckWin();
+		if (winner != EMPTY) {
+			if (winner == CIRCLE) {
+				MessageBox(GetHWnd(), _T("Player O Wins!"), _T("Game Over"), MB_OK);
+			}
+			else {
+				MessageBox(GetHWnd(), _T("Player X Wins!"), _T("Game Over"), MB_OK);
+			}
+		}
+		else {
+			if (!tictactoe.ExistGridEmpty()) {
+				MessageBox(GetHWnd(), _T("Draw! No Winner!"), _T("Game Over"), MB_OK);
+			}
+		}
 
 		// 绘制
+		cleardevice();
 		FlushBatchDraw();
 	}
 
